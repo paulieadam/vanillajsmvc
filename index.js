55f44f8e6249c83567c6036e84bd44b9d73e9117
paulie <paul.adam@live.se>
2019-10-03 21:01:15 +0200

    Add methods to app to handle components

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 import App from "./lib/App.js";
 
 const app = new App("#app");
+
+app.addComponent({
+    name: "albums",
+    model: {
+        albums: []
+    },
+    view(model) {
+        return `<h2>Number of albums ${model.albums.length}`;
+    }
+});
+
+app.showComponent("albums");
